07ad865b6360def49a125772c5faa165046d6e0a
Kmenguete <menguetekevin@gmail.com>
2023-02-21 15:17:24 +0100

    Successfully moved Profile model from oc_lettings_site to profiles app

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 import lettings.views
+import profiles.views
 import oc_lettings_site.views
 
 urlpatterns = [
     path('', oc_lettings_site.views.index, name='index'),
     path('lettings/', lettings.views.lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', lettings.views.letting, name='letting'),
-    path('profiles/', oc_lettings_site.views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', oc_lettings_site.views.profile, name='profile'),
+    path('profiles/', profiles.views.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
