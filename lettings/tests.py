77b975413f16581b647e2c2a6e40bde43d271a6a
Kmenguete <menguetekevin@gmail.com>
2023-03-13 11:18:36 +0100

    Every tests passed and updated requirements.txt

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,82 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from .models import Letting, Address
+from pytest_django.asserts import assertTemplateUsed, assertQuerysetEqual
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_index_view():
+    client = Client()
+    address_1 = Address.objects.create(number=250, street="Saint-Mary",
+                                       city="Chicago", state="Illinois",
+                                       zip_code=309, country_iso_code="USA")
+    address_2 = Address.objects.create(number=100, street="Pierre-fonds",
+                                       city="San Francisco", state="California",
+                                       zip_code=500, country_iso_code="USA")
+    address_3 = Address.objects.create(number=60, street="Glenwood",
+                                       city="New York City", state="New York",
+                                       zip_code=101, country_iso_code="USA")
+    letting_1 = Letting.objects.create(title="The crystal palace", address=address_1)
+    letting_2 = Letting.objects.create(title="Yan's manor", address=address_2)
+    letting_3 = Letting.objects.create(title="Al Riyad", address=address_3)
+    lettings_list = [letting_1, letting_2, letting_3]
+    path = reverse("lettings:index")
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = '<title>Lettings</title>\n' \
+                       '<h1>Lettings</h1>\n' \
+                       '\n' \
+                       '    <ul>\n' \
+                       '    \n' \
+                       '        <li>\n ' \
+                       '           <a href="/lettings/1/">\n ' \
+                       '               The crystal palace\n ' \
+                       '           </a>\n' \
+                       '        </li>\n ' \
+                       '   \n ' \
+                       '       <li>\n ' \
+                       '           <a href="/lettings/2/">\n  ' \
+                       '              Yan&#x27;s manor\n  ' \
+                       '          </a>\n   ' \
+                       '     </li>\n' \
+                       '    \n ' \
+                       '       <li>\n ' \
+                       '           <a href="/lettings/3/">\n    ' \
+                       '            Al Riyad\n  ' \
+                       '          </a>\n   ' \
+                       '     </li>\n ' \
+                       '   \n ' \
+                       '   </ul>\n' \
+                       '\n' \
+                       '<div><a href="/">Home</a></div>\n' \
+                       '<div><a href="/profiles/">Profiles</a></div>'
+
+    assert content == expected_content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/index.html")
+    assertQuerysetEqual(response.context['lettings_list'], lettings_list, ordered=False)
+
+
+@pytest.mark.django_db
+def test_letting_view():
+    client = Client()
+    address = Address.objects.create(number=250, street="Saint-Mary",
+                                     city="Chicago", state="Illinois",
+                                     zip_code=309, country_iso_code="USA")
+    letting = Letting.objects.create(title="The crystal palace", address=address)
+    path = reverse("lettings:letting", kwargs={"letting_id": letting.pk})
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = '<title>The crystal palace</title>\n' \
+                       '<h1>The crystal palace</h1>\n' \
+                       '<p>250 Saint-Mary</p>\n' \
+                       '<p>Chicago, Illinois 309</p>\n' \
+                       '<p>USA</p>\n' \
+                       '<div><a href="/lettings/">Back</a></div>\n' \
+                       '<div><a href="/">Home</a></div>\n' \
+                       '<div><a href="/profiles/">Profiles</a></div>'
+
+    assert content == expected_content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "lettings/letting.html")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,80 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from .models import Profile
+from pytest_django.asserts import assertTemplateUsed, assertQuerysetEqual
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_index_view():
+    client = Client()
+    user_1 = User.objects.create(username="Matthieu", first_name="Matt",
+                                 last_name="Jammy", email="mattjam@gmail.com")
+    user_2 = User.objects.create(username="Elodie", first_name="Elodie",
+                                 last_name="Belem", email="elodiebelem@gmail.com")
+    user_3 = User.objects.create(username="Vanessa", first_name="Vanessa",
+                                 last_name="Britz", email="vbritz@gmail.com")
+    profile_1 = Profile.objects.create(user=user_1, favorite_city="Chicago")
+    profile_2 = Profile.objects.create(user=user_2, favorite_city="San Francisco")
+    profile_3 = Profile.objects.create(user=user_3, favorite_city="New York City")
+    profiles_list = [profile_1, profile_2, profile_3]
+    path = reverse("profiles:index")
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = '<title>Profiles</title>\n' \
+                       '<h1>Profiles</h1>\n' \
+                       '\n' \
+                       '    <ul>\n ' \
+                       '   \n' \
+                       '        <li>\n ' \
+                       '           <a href="/profiles/Matthieu/">\n ' \
+                       '               Matthieu\n ' \
+                       '           </a>\n' \
+                       '        </li>\n' \
+                       '    \n ' \
+                       '       <li>\n ' \
+                       '           <a href="/profiles/Elodie/">\n  ' \
+                       '              Elodie\n  ' \
+                       '          </a>\n  ' \
+                       '      </li>\n' \
+                       '    \n  ' \
+                       '      <li>\n  ' \
+                       '          <a href="/profiles/Vanessa/">\n    ' \
+                       '            Vanessa\n   ' \
+                       '         </a>\n  ' \
+                       '      </li>\n ' \
+                       '   \n   ' \
+                       ' </ul>\n' \
+                       '\n' \
+                       '<div><a href="/">Home</a></div>\n' \
+                       '<div><a href="/lettings/">Lettings</a></div>'
+
+    assert content == expected_content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "profiles/index.html")
+    assertQuerysetEqual(response.context['profiles_list'], profiles_list, ordered=False)
+
+
+@pytest.mark.django_db
+def test_letting_view():
+    client = Client()
+    user = User.objects.create(username="Matthieu", first_name="Matt",
+                               last_name="Jammy", email="mattjam@gmail.com")
+    profile = Profile.objects.create(user=user, favorite_city="Seattle")
+    path = reverse("profiles:profile", kwargs={"username": profile.user.username})
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = '<title>Matthieu</title>\n' \
+                       '<h1>Matthieu</h1>\n' \
+                       '<p>First name: Matt</p>\n' \
+                       '<p>Last name: Jammy</p>\n' \
+                       '<p>Email: mattjam@gmail.com</p>\n' \
+                       '<p>Favorite city: Seattle</p>\n' \
+                       '<div><a href="/profiles/">Back</a></div>\n' \
+                       '<div><a href="/">Home</a></div>\n' \
+                       '<div><a href="/lettings/">Lettings</a></div>'
+
+    assert content == expected_content
+    assert response.status_code == 200
+    assertTemplateUsed(response, "profiles/profile.html")
